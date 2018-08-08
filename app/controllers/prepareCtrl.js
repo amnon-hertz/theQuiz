13f33e12bf4217196ed67ed8367f62856d5ec85b
eyalsh59 <eyal.shkolnik@gmail.com>
2018-08-08 19:16:55 +0300

    Merge

--- app/controllers/prepareCtrl.js
+++ app/controllers/prepareCtrl.js
@@ -10,12 +10,7 @@ app.controller("prepareCtrl", function ($rootScope, $http, $scope, $location, $i
   $scope.selectedQuestion = null;
   $scope.quizes = [];
   $scope.questions = [];
-<<<<<<< HEAD
   $scope.activateText = 'הפעל חידון';
-=======
-  $scope.activateText = 'Activate Quiz';
-  clearInterval($rootScope.intrvl);
->>>>>>> 78065a0cb013245a3a767b3d5aaba3a4b2118238
 
   $http.get($rootScope.linkJson + '/quiz').then(function (response) {
     for (i = 0; i < response.data.length; i++) {
